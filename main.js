bcc1909b223306aaa31db06acd16370429367a1f
Amirhon <amirhonmalikov10@gmail.com>
2023-12-20 22:15:54 +0500

    js1

---
+++ main.js
@@ -0,0 +1,38 @@
+var myWord = prompt('Введите свое имя '),
+    myAge = +prompt('Сколько вам лет');
+
+console.log( 'Вас зовут ' + myWord);
+console.log( 'Мне ' + myAge);
+  
+ 
+var plus = +prompt('Решите пример: 10 + 5');
+console.log('10 + 5 = ' + (10 + 5 ));
+console.log('Ваш ответ ' + plus);
+
+var minus = +prompt('Решите пример: 10 - 5');
+console.log('10 - 5 = ' + (10 - 5 ));
+console.log('Ваш ответ ' + minus);
+
+var multiply = +prompt('Решите пример: 10 * 5');
+console.log('10 * 5 = ' + 10 * 5 );
+console.log('Ваш ответ ' + multiply);
+
+var divide = +prompt('Решите пример: 10 / 5');
+console.log('10 / 5 = ' + 10 / 5 );
+console.log('Ваш ответ ' + divide);
+
+var procent = +prompt('Решите пример: 10 % 5');
+console.log('10 % 5 = ' + 10 % 5 );
+console.log('Ваш ответ ' + procent);
+
+ 
+ 
+
+
+
+
+// var x = +prompt('введите первое число ');
+// var y = +prompt('введите второе число');
+// var z = +prompt('введите третье число');
+
+// alert( x + y + z / 3);
